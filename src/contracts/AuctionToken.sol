1a75fc3c8909d8bbb335edbf9f5a7fb9be6a4c9d
anasshad <anasshad@hotmail.com>
2021-03-15 15:06:08 +0500

    auction tests

--- src/contracts/AuctionToken.sol
+++ src/contracts/AuctionToken.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
 
-contract AuctionToken is IERC721Receiver{
+contract AuctionToken is ERC721Holder {
+    string public name;
     ERC721 public nft;
-    
-    struct Auction{
+
+    struct Auction {
         address seller;
         uint128 price;
         uint256 startTime;
@@ -16,68 +17,81 @@ contract AuctionToken is IERC721Receiver{
         uint256 highestBid;
         address highestBidder;
     }
-    
+
     mapping(uint256 => Auction) public tokenIdToAuction;
-    
-    
+
     constructor(address _nftAddress) {
         nft = ERC721(_nftAddress);
+        name = "AuctionToken";
     }
-    
+
     event AuctionCreated(uint256 _tokenId);
     event BidPosted(uint256 _bidAmount, address indexed bidder);
-    
-    function createAuction(uint256 _tokenId, uint128 _price, uint256 _startTime, uint256 _endTime) public{
-        require(msg.sender == nft.ownerOf(_tokenId), 'Should be the owner of token');
+
+    function createAuction(
+        uint256 _tokenId,
+        uint128 _price,
+        uint256 _startTime,
+        uint256 _endTime
+    ) public {
+        require(
+            msg.sender == nft.ownerOf(_tokenId),
+            "Should be the owner of token"
+        );
         require(_startTime >= block.timestamp);
         require(_endTime >= block.timestamp);
         require(_endTime > _startTime);
+
         nft.safeTransferFrom(msg.sender, address(this), _tokenId);
-        Auction memory auction = Auction({
-            seller: msg.sender,
-            price: _price,
-            startTime: _startTime,
-            endTime: _endTime,
-            highestBid: 0,
-            highestBidder: address(0x0)
-        });
+        Auction memory auction =
+            Auction({
+                seller: msg.sender,
+                price: _price,
+                startTime: _startTime,
+                endTime: _endTime,
+                highestBid: 0,
+                highestBidder: address(0x0)
+            });
         tokenIdToAuction[_tokenId] = auction;
         emit AuctionCreated(_tokenId);
     }
-    
-    function bid(uint256 _tokenId) public payable{
+
+    function bid(uint256 _tokenId) public payable {
         require(isBidValid(_tokenId, msg.value));
         Auction memory auction = tokenIdToAuction[_tokenId];
         uint256 highestBid = auction.highestBid;
-        if(msg.value > highestBid){
+        if (msg.value > highestBid) {
             tokenIdToAuction[_tokenId].highestBid = msg.value;
             tokenIdToAuction[_tokenId].highestBidder = msg.sender;
             emit BidPosted(msg.value, msg.sender);
         }
     }
-    
-    function finalize(uint256 _tokenId, address _to) public{
+
+    function finalize(uint256 _tokenId, address _to) public {
         Auction memory auction = tokenIdToAuction[_tokenId];
-        require(msg.sender == auction.seller, 'Should only be called by the seller');
+        require(
+            msg.sender == auction.seller,
+            "Should only be called by the seller"
+        );
         require(block.timestamp >= auction.endTime);
         nft.safeTransferFrom(address(this), _to, _tokenId);
         delete tokenIdToAuction[_tokenId];
     }
-    
-    function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
-        return this.onERC721Received.selector;
-    }
-    
-    function isBidValid(uint256 _tokenId, uint256 _bidAmount) internal view returns (bool){
+
+    function isBidValid(uint256 _tokenId, uint256 _bidAmount)
+        internal
+        view
+        returns (bool)
+    {
         Auction memory auction = tokenIdToAuction[_tokenId];
         uint256 startTime = auction.startTime;
         uint256 endTime = auction.endTime;
         address seller = auction.seller;
         uint128 price = auction.price;
-        bool withinTime = block.timestamp <= startTime && block.timestamp <=endTime;
+        bool withinTime =
+            block.timestamp <= startTime && block.timestamp <= endTime;
         bool bidAmountValid = _bidAmount >= price;
         bool sellerValid = seller != address(0);
-        return withinTime && bidAmountValid&& sellerValid;
+        return withinTime && bidAmountValid && sellerValid;
     }
-
 }
---
+++ test/auction_token.js
@@ -0,0 +1,64 @@
+const { assert } = require("chai");
+
+const AuctionToken = artifacts.require("AuctionToken");
+const GradientToken = artifacts.require("GradientToken");
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("AuctionToken", function(accounts) {
+  let auctionToken, gradientToken;
+  let tokenId;
+
+  before(async () => {
+    gradientToken = await GradientToken.deployed();
+    auctionToken = await AuctionToken.deployed(gradientToken.address);
+  });
+
+  describe("Deployment", async () => {
+    it("has a name", async () => {
+      const name = await auctionToken.name();
+      assert.equal(name, "AuctionToken");
+    });
+  });
+
+  describe("Auction creation", async () => {
+    it("creates an auction", async () => {
+      const tokenMinted = await gradientToken.mint("#A8E99A", "#8A3C99", {
+        from: accounts[0],
+      });
+      tokenId = tokenMinted.logs[0].args.tokenId;
+
+      gradientToken.approve(auctionToken.address, tokenId, {
+        from: accounts[0],
+      });
+      const result = await auctionToken.createAuction(
+        tokenId,
+        web3.utils.toWei("1", "ether"),
+        Date.now(),
+        Date.now() + 86400,
+        { from: accounts[0] }
+      );
+      assert.equal(
+        result.logs[0].args._tokenId.toString(),
+        tokenId.toString(),
+        "Auction should be created with correct tokenId"
+      );
+    });
+  });
+
+  describe("Bidding", async () => {
+    it("creates a new bid", async () => {
+      const result = await auctionToken.bid(tokenId, {
+        value: web3.utils.toWei("1.2", "ether"),
+        from: accounts[1],
+      });
+      assert.equal(
+        result.logs[0].args._bidAmount,
+        web3.utils.toWei("1.2", "ether")
+      );
+      assert.equal(result.logs[0].args.bidder, accounts[1]);
+    });
+  });
+});
